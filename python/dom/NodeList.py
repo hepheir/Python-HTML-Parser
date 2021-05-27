9e04f639c1bb654508ce31ed5fcdc680cd2d76d4
김동주 <hepheir@gmail.com>
2021-05-27 23:15:51 +0900

    Add method `item`

--- python/dom/NodeList.py
+++ python/dom/NodeList.py
@@ -1,3 +1,10 @@
+from __future__ import absolute_import
+
+from typing import Optional
+
+from python.dom.type_checking import AnyNode
+
+
 class NodeList(list):
     """Interface NodeList
 
@@ -11,3 +18,17 @@ class NodeList(list):
         """The number of nodes in the list. The range of valid child node indices is 0 to `length`-1 inclusive.
         """
         return len(self)
+
+    def item(self, index: int) -> Optional[AnyNode]:
+        """Returns the indexth item in the collection. If index is greater than or equal to the number of nodes in the list, this returns `None`.
+
+        Args:
+            index: Index into the collection.
+
+        Returns:
+            The node at the `index`th position in the NodeList, or `None` if that is not a valid index.
+        """
+        if 0 <= index < self.length:
+            return self[index]
+        else:
+            return None
