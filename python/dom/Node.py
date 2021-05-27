066dbcaf3e4efa17c06cfc3d81638d5150ae441f
김동주 <hepheir@gmail.com>
2021-05-27 10:22:39 +0900

    Add readonly attribute `node_type`

--- python/dom/Node.py
+++ python/dom/Node.py
@@ -1,4 +1,7 @@
 import enum
+from typing import AnyStr
+
+from python.dom.DOMString import DOMString
 
 
 class NodeType(enum.IntEnum):
@@ -25,7 +28,7 @@ class Node:
 
     The `Node` interface is the primary datatype for the entire Document Object Model. It represents a single node in the document tree. While all objects implementing the `Node` interface expose methods for dealing with children, not all objects implementing the `Node` interface may have children. For example, `Text` nodes may not have children, and adding children to such nodes results in a `DOMException` being raised.
 
-    The attributes `nodeName`, `nodeValue` and attributes are included as a mechanism to get at node information without casting down to the specific derived interface. In cases where there is no obvious mapping of these attributes for a specific `nodeType` (e.g., `nodeValue` for an Element or `attributes` for a Comment), this returns `None`. Note that the specialized interfaces may contain additional and more convenient mechanisms to get and set the relevant information.
+    The attributes `nodeName`, `nodeValue` and attributes are included as a mechanism to get at node information without casting down to the specific derived interface. In cases where there is no obvious mapping of these attributes for a specific `node_type` (e.g., `nodeValue` for an Element or `attributes` for a Comment), this returns `None`. Note that the specialized interfaces may contain additional and more convenient mechanisms to get and set the relevant information.
     """
     ATTRIBUTE_NODE = NodeType.ATTRIBUTE_NODE
     TEXT_NODE = NodeType.TEXT_NODE
@@ -40,5 +43,12 @@ class Node:
     DOCUMENT_FRAGMENT_NODE = NodeType.DOCUMENT_FRAGMENT_NODE
     NOTATION_NODE = NodeType.NOTATION_NODE
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+                 node_type: NodeType) -> None:
+        self._node_type: NodeType = node_type
+
+    @property
+    def node_type(self) -> NodeType:
+        """A code representing the type of the underlying object.
+        """
+        return self._node_type
