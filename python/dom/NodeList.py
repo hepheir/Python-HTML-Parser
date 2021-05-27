687a8fdb792c87e08763faf254b308c8e38c6eb5
김동주 <hepheir@gmail.com>
2021-05-27 23:13:28 +0900

    Make class `NodeList` more list-like by inheriting `list`

--- python/dom/NodeList.py
+++ python/dom/NodeList.py
@@ -1,4 +1,4 @@
-class NodeList:
+class NodeList(list):
     """Interface NodeList
 
     The `NodeList` interface provides the abstraction of an ordered collection of nodes, without defining or constraining how this collection is implemented.
