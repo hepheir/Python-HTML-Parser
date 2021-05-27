bfc687853237de7229357cc3c0cd6f647fa65d2b
김동주 <hepheir@gmail.com>
2021-05-27 10:26:40 +0900

    Add readonly attribute `node_name` (abstract)

--- python/dom/Node.py
+++ python/dom/Node.py
@@ -28,7 +28,7 @@ class Node:
 
     The `Node` interface is the primary datatype for the entire Document Object Model. It represents a single node in the document tree. While all objects implementing the `Node` interface expose methods for dealing with children, not all objects implementing the `Node` interface may have children. For example, `Text` nodes may not have children, and adding children to such nodes results in a `DOMException` being raised.
 
-    The attributes `nodeName`, `nodeValue` and attributes are included as a mechanism to get at node information without casting down to the specific derived interface. In cases where there is no obvious mapping of these attributes for a specific `node_type` (e.g., `nodeValue` for an Element or `attributes` for a Comment), this returns `None`. Note that the specialized interfaces may contain additional and more convenient mechanisms to get and set the relevant information.
+    The attributes `node_name`, `nodeValue` and attributes are included as a mechanism to get at node information without casting down to the specific derived interface. In cases where there is no obvious mapping of these attributes for a specific `node_type` (e.g., `nodeValue` for an Element or `attributes` for a Comment), this returns `None`. Note that the specialized interfaces may contain additional and more convenient mechanisms to get and set the relevant information.
     """
     ATTRIBUTE_NODE = NodeType.ATTRIBUTE_NODE
     TEXT_NODE = NodeType.TEXT_NODE
@@ -46,6 +46,12 @@ class Node:
     def __init__(self,
                  node_type: NodeType) -> None:
         self._node_type: NodeType = node_type
+
+    @property
+    def node_name(self) -> DOMString:
+        """The name of this node, depending on its type.
+        """
+        raise NotImplementedError
 
     @property
     def node_type(self) -> NodeType:
