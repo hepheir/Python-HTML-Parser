e7448718ae319fab704d70a7f66431ceb4a1014e
Hepheir <hepheir@gmail.com>
2021-05-31 18:52:46 +0900

    Create fundamental_interface.py

---
+++ w3/python/core/fundamental_interfaces/__init__.py
@@ -0,0 +1,4 @@
+"""Fundamental Interfaces
+
+The interfaces within this section are considered fundamental, and must be fully implemented by all conforming implementations of the DOM, including all HTML DOM implementations.
+"""
