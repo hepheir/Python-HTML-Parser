55118b0028a154f1bf0ccf3f81bedf5e95a042d3
Hepheir <hepheir@gmail.com>
2021-05-31 19:53:28 +0900

    Add some details for "w3/__init__.py"

---
+++ w3/__init__.py
@@ -0,0 +1,14 @@
+"""A Python HTML Parser
+
+This is a personal project owned by Hepheir.
+
+https://github.com/Hepheir/Python-HTML-Parser/
+"""
+
+from w3.python import dom
+from w3.python import parser
+
+
+__version__ = '0.0.0'
+
+__author__ = 'hepheir@gmail.com'
