131bc9286a44223b6d406f6583e0be94560ad0f3
Hepheir <hepheir@gmail.com>
2021-05-31 19:16:58 +0900

    Fix TypeError: "cannot extend enumerations"

--- w3/python/core/fundamental_interfaces/DOMException.py
+++ w3/python/core/fundamental_interfaces/DOMException.py
@@ -17,11 +17,20 @@ class ExceptionCode(enum.IntEnum):
     INUSE_ATTRIBUTE_ERR = 10
 
 
-class DOMException(Exception, ExceptionCode):
+class DOMException(Exception):
     """Exception DOMException
 
     DOM operations only raise exceptions in "exceptional" circumstances, i.e., when an operation is impossible to perform (either for logical reasons, because data is lost, or because the implementation has become unstable). In general, DOM methods return specific error values in ordinary processing situation, such as out-of-bound errors when using `NodeList`.
     Implementations may raise other exceptions under other circumstances. For example, implementations may raise an implementation-dependent exception if a null argument is passed.
     Some languages and object systems do not support the concept of exceptions. For such systems, error conditions may be indicated using native error reporting mechanisms. For some bindings, for example, methods may return error codes similar to those listed in the corresponding method descriptions.
     """
-    pass
+    INDEX_SIZE_ERR = ExceptionCode.INDEX_SIZE_ERR
+    DOMSTRING_SIZE_ERR = ExceptionCode.DOMSTRING_SIZE_ERR
+    HIERARCHY_REQUEST_ERR = ExceptionCode.HIERARCHY_REQUEST_ERR
+    WRONG_DOCUMENT_ERR = ExceptionCode.WRONG_DOCUMENT_ERR
+    INVALID_CHARACTER_ERR = ExceptionCode.INVALID_CHARACTER_ERR
+    NO_DATA_ALLOWED_ERR = ExceptionCode.NO_DATA_ALLOWED_ERR
+    NO_MODIFICATION_ALLOWED_ERR = ExceptionCode.NO_MODIFICATION_ALLOWED_ERR
+    NOT_FOUND_ERR = ExceptionCode.NOT_FOUND_ERR
+    NOT_SUPPORTED_ERR = ExceptionCode.NOT_SUPPORTED_ERR
+    INUSE_ATTRIBUTE_ERR = ExceptionCode.INUSE_ATTRIBUTE_ERR
