fc13453628a12fce8a04efdf7f60ac9767fa4da7
Hepheir <hepheir@gmail.com>
2021-05-31 19:45:57 +0900

    Create interface `DOMImplementation`

---
+++ w3/python/core/fundamental_interface/DOMImplementation.py
@@ -0,0 +1,8 @@
+class DOMImplementation:
+    """Interface DOMImplementation
+
+    The `DOMImplementation` interface provides a number of methods for performing operations that are independent of any particular instance of the document object model.
+
+    The DOM Level 1 does not specify a way of creating a document instance, and hence document creation is an operation specific to an implementation. Future Levels of the DOM specification are expected to provide methods for creating documents directly.
+    """
+    pass
