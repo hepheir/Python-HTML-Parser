f56d30ba6390f3a87f39add9cedbdfae9b084a85
Hepheir <hepheir@gmail.com>
2021-05-31 20:31:02 +0900

    Create interface `Node`

--- w3/python/core/fundamental_interface/Node.py
+++ w3/python/core/fundamental_interface/Node.py
@@ -32,3 +32,25 @@ class NodeType(enum.IntEnum):
     DOCUMENT_TYPE_NODE = 10
     DOCUMENT_FRAGMENT_NODE = 11
     NOTATION_NODE = 12
+
+
+class Node:
+    """Interface `Node`
+
+    The `Node` interface is the primary datatype for the entire Document Object Model. It represents a single node in the document tree. While all objects implementing the `Node` interface expose methods for dealing with children, not all objects implementing the `Node` interface may have children. For example, `Text` nodes may not have children, and adding children to such nodes results in a `DOMException` being raised.
+    The attributes `node_name`, `node_value` and attributes are included as a mechanism to get at node information without casting down to the specific derived interface. In cases where there is no obvious mapping of these attributes for a specific `node_type` (e.g., `node_value` for an Element or `attributes` for a Comment), this returns `None`. Note that the specialized interfaces may contain additional and more convenient mechanisms to get and set the relevant information.
+
+    Attributes:
+        node_name: The name of this node, depending on its type.
+        node_value: The value of this node, depending on its type.
+        node_type: A code representing the type of the underlying object.
+        parent_node: The parent of this node.
+        child_nodes: A `NodeList` that contains all children of this node.
+        first_child: The first child of this node.
+        last_child: The last child of this node.
+        previous_sibling: The node immediately preceding this node.
+        next_sibling: The node immediately following this node.
+        attributes: A `NamedNodeMap` containing the attributes of this node.
+        owner_document: The `Document` object associated with this node.
+    """
+    pass
