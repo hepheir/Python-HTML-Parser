a5b76562f4f0ad6564a3c6adb7b733e5fdcf98e5
Hepheir <hepheir@gmail.com>
2021-06-06 13:32:10 +0900

    Update unittests (#16, #17)

--- w3/python/core/fundamental_interface/NodeList_test.py
+++ w3/python/core/fundamental_interface/NodeList_test.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Iterator, Optional
 import unittest
 
 from w3.dom import Node
@@ -6,7 +6,20 @@ from w3.dom import NodeType
 from w3.dom import NodeList
 
 
-def _make_nodes(n: int) -> Iterator[Node]:
+def _create_document_node():
+    return Node(node_type=NodeType.DOCUMENT_NODE,
+                node_name='#document',
+                owner_document=None)
+
+
+def _create_text_node(document: Node):
+    assert document.node_type == NodeType.DOCUMENT_NODE
+    return Node(node_type=NodeType.TEXT_NODE,
+                node_name='#text',
+                owner_document=document)
+
+
+def _make_nodes(n: int, document: Optional[Node] = None) -> Iterator[Node]:
     """Accessor to create `n` random nodes immediatly.
 
     Args:
@@ -15,17 +28,21 @@ def _make_nodes(n: int) -> Iterator[Node]:
     Yields:
         A randomly generated node.
     """
+    if document is None:
+        document = _create_document_node()
     for _ in range(n):
-        yield Node(NodeType.TEXT_NODE, '#text')
+        yield _create_text_node(document)
 
 
-class Test_NodeList(unittest.TestCase):
-    def testInit(self):
+class Test_Dunder(unittest.TestCase):
+    def testInit_Empty(self):
         NodeList()
-        # Initiate with a node
+
+    def testInit_SingleNode(self):
         node = next(_make_nodes(1))
         NodeList([node])
-        # Initiate with multiple nodes.
+
+    def testInit_MultipleNodes(self):
         NodeList(_make_nodes(4))
 
     def testIter(self):
@@ -51,38 +68,55 @@ class Test_NodeList(unittest.TestCase):
             self.assertEqual(node_list[i], nodes[i])
 
     def testSetItem(self):
-        node = Node(NodeType.TEXT_NODE,
-                    '#text')
-        node_list = NodeList(_make_nodes(4))
-        node_list[2] = node
-        self.assertEqual(node_list[2], node)
+        document = _create_document_node()
+        text_node = _create_text_node(document)
+        node_list = NodeList(_make_nodes(4, document))
+        node_list[2] = text_node
+        self.assertEqual(node_list[2], text_node)
 
-    def testItem_inSize(self):
+    def testContains(self):
+        nodes = [*_make_nodes(4)]
+        node_list = NodeList(nodes)
+        for n in node_list:
+            self.assertIn(n, node_list)
+
+    def testContains_NotIn(self):
+        internal_nodes = [*_make_nodes(4)]
+        external_node = _make_nodes(1)
+        node_list = NodeList(internal_nodes)
+        self.assertNotIn(external_node, node_list)
+
+
+class Test_Length(unittest.TestCase):
+    def test_Empty(self):
+        node_list = NodeList()
+        self.assertEqual(node_list.length, 0)
+
+    def test_OneItem(self):
+        node_list = NodeList(_make_nodes(1))
+        self.assertEqual(node_list.length, 1)
+
+    def test_SomeItems(self):
+        node_list = NodeList(_make_nodes(5))
+        self.assertEqual(node_list.length, 5)
+
+
+class Test_Item(unittest.TestCase):
+    def test_InSize(self):
         nodes = [*_make_nodes(5)]
         node_list = NodeList(nodes)
         for i in range(5):
             self.assertEqual(node_list.item(i), nodes[i])
 
-    def testItem_greaterOrEqualThanSize(self):
+    def test_GreaterOrEqualThanSize(self):
         node_list = NodeList(_make_nodes(5))
         for i in range(5, 8):
             self.assertIsNone(node_list.item(i))
 
-    def testItem_notValid(self):
+    def test_InvalidIndex(self):
         node_list = NodeList(_make_nodes(5))
         for idx in [-1, -9999, 'hi', '1', None, Node, NodeList, b'0']:
             self.assertIsNone(node_list.item(idx))
-
-    def testLength(self):
-        # Empty NodeList
-        node_list = NodeList()
-        self.assertEqual(node_list.length, 0)
-        # NodeList with a node.
-        node_list = NodeList(_make_nodes(1))
-        self.assertEqual(node_list.length, 1)
-        # NodeList with some items.
-        node_list = NodeList(_make_nodes(5))
-        self.assertEqual(node_list.length, 5)
 
 
 if __name__ == '__main__':
--- w3/python/core/fundamental_interface/Node_test.py
+++ w3/python/core/fundamental_interface/Node_test.py
@@ -5,72 +5,222 @@ from w3.dom import Node
 from w3.dom import NodeType
 
 
-class Test_Node(unittest.TestCase):
-    def test_init(self):
-        node = Node(node_type=NodeType.DOCUMENT_NODE,
-                    node_name='#document',
-                    read_only=False)
-        self.assertEqual(node.node_type, NodeType.DOCUMENT_NODE)
-        self.assertEqual(node.node_name, '#document')
-        self.assertEqual(node.node_value, '')
+def _create_document_node():
+    return Node(node_type=NodeType.DOCUMENT_NODE,
+                node_name='#document',
+                owner_document=None)
 
-    def testNodeValue_get(self):
-        node = Node(node_type=NodeType.TEXT_NODE,
+
+def _create_element_node(document: Node):
+    assert document.node_type == NodeType.DOCUMENT_NODE
+    return Node(node_type=NodeType.ELEMENT_NODE,
+                node_name='tagName',
+                owner_document=document)
+
+
+class Test_Dunder(unittest.TestCase):
+    def testInit(self):
+        # Creating document node.
+        document = Node(node_type=NodeType.DOCUMENT_NODE,
+                        node_name='#document',
+                        owner_document=None,
+                        read_only=False)
+        self.assertEqual(document.node_type, NodeType.DOCUMENT_NODE)
+        self.assertEqual(document.node_name, '#document')
+        self.assertEqual(document.node_value, '')
+
+
+class Test_NodeValue(unittest.TestCase):
+    def testGetter(self):
+        document = _create_document_node()
+        text = Node(node_type=NodeType.TEXT_NODE,
                     node_name='#text',
+                    owner_document=document,
                     node_value='lorem ipsum')
-        self.assertEqual(node.node_value, 'lorem ipsum')
+        self.assertEqual(text.node_value, 'lorem ipsum')
 
-    def testNodeValue_set(self):
-        # set value from constructor
-        node = Node(node_type=NodeType.TEXT_NODE,
+    def testConstructor(self):
+        document = _create_document_node()
+        text = Node(node_type=NodeType.TEXT_NODE,
                     node_name='#text',
+                    owner_document=document,
                     node_value='foo',
                     read_only=False)
-        self.assertEqual(node.node_value, 'foo')
-        # set using setter
-        node.node_value = 'bar'
-        self.assertEqual(node.node_value, 'bar')
+        self.assertEqual(text.node_value, 'foo')
 
-    def testNodeValue_setReadOnly(self):
-        node = Node(node_type=NodeType.TEXT_NODE,
+    def testSetter(self):
+        document = _create_document_node()
+        text = Node(node_type=NodeType.TEXT_NODE,
                     node_name='#text',
+                    owner_document=document,
+                    read_only=False)
+        text.node_value = 'bar'
+        self.assertEqual(text.node_value, 'bar')
+
+    def testSetter_ReadOnly(self):
+        document = _create_document_node()
+        text = Node(node_type=NodeType.TEXT_NODE,
+                    node_name='#text',
+                    owner_document=document,
                     node_value='foo',
                     read_only=True)
         try:
-            node.node_value = 'bar'
+            text.node_value = 'bar'
         except DOMException as e:
             code = e.args[0]
             self.assertEqual(code, DOMException.NO_MODIFICATION_ALLOWED_ERR)
         else:
             self.fail()
         # `node_value` should not be modified.
-        self.assertEqual(node.node_value, 'foo')
+        self.assertEqual(text.node_value, 'foo')
 
-    def testNodeType_get(self):
+
+class Test_NodeType(unittest.TestCase):
+    def testGetter(self):
+        document = _create_document_node()
         for node_type in NodeType:
             node = Node(node_type=node_type,
-                        node_name='')
+                        node_name='',
+                        owner_document=document)
             self.assertEqual(node.node_type, node_type)
 
-    def testNodeType_setCorrectTypes(self):
+    def testSetter_CorrectTypes(self):
+        document = _create_document_node()
         for node_type in NodeType:
-            node = Node(node_type=node_type,
-                        node_name='')
-            self.assertEqual(node.node_type, node_type)
+            Node(node_type=node_type,
+                 node_name='',
+                 owner_document=document)
 
-    def testNodeType_setWrongTypes(self):
-        for node_type in [-1, None, 'hi', Node, True, NodeType, 'DOCUMENT_NODE']:
-            with self.assertRaises(TypeError):
+    def testSetter_WrongTypes(self):
+        document = _create_document_node()
+        for node_type in [-1, None, 'hi', True, Node, NodeType, 'DOCUMENT_NODE']:
+            try:
                 Node(node_type=node_type,
-                     node_name='')
+                     node_name='',
+                     owner_document=document)
+                self.fail()
+            except Exception:
+                pass
 
-    def testParentNode_get(self):
-        parent_node = Node(node_type=NodeType.DOCUMENT_FRAGMENT_NODE,
-                           node_name='#document-fragment')
-        node = Node(node_type=NodeType.ELEMENT_NODE,
-                    node_name='tagName',
-                    parent_node=parent_node)
-        self.assertEqual(node.parent_node, parent_node)
+
+class Test_ParentNode(unittest.TestCase):
+    def testConstrutor(self):
+        document = _create_document_node()
+        docfrag = Node(node_type=NodeType.DOCUMENT_FRAGMENT_NODE,
+                       node_name='#document-fragment',
+                       owner_document=document)
+        self.assertIsNone(docfrag.parent_node)
+
+    def testGetter(self):
+        document = _create_document_node()
+        parent_node = _create_element_node(document)
+        child_node = _create_element_node(document)
+        # Warning: this is assuming that `append_child()` method works properly.
+        parent_node.append_child(child_node)
+        self.assertEqual(child_node.parent_node, parent_node)
+
+
+class Test_InsertBefore(unittest.TestCase):
+    def test_WithoutRefNode(self):
+        document = _create_document_node()
+        parent_node = _create_element_node(document)
+        child_node = _create_element_node(document)
+        # Testing
+        parent_node.insert_before(child_node)
+        self.assertEqual(child_node.parent_node, parent_node)
+        self.assertEqual(parent_node.child_nodes.item(0), child_node)
+        self.assertEqual(parent_node.child_nodes.length, 1)
+
+    def test_WithRefNode(self):
+        document = _create_document_node()
+        parent_node = _create_element_node(document)
+        first_child_node = _create_element_node(document)
+        second_child_node = _create_element_node(document)
+        # Warning: this is assuming that `append_child()` method works properly.
+        parent_node.append_child(second_child_node)
+        # Testing
+        parent_node.insert_before(first_child_node, second_child_node)
+        self.assertEqual(first_child_node.parent_node, parent_node)
+        self.assertEqual(parent_node.child_nodes.item(0), first_child_node)
+        self.assertEqual(parent_node.child_nodes.item(1), second_child_node)
+        self.assertEqual(parent_node.child_nodes.length, 2)
+
+    def test_InsertExistingChild(self):
+        document = _create_document_node()
+        parent_node = _create_element_node(document)
+        first_child_node = _create_element_node(document)
+        second_child_node = _create_element_node(document)
+        # Warning: this is assuming that `append_child()` method works properly.
+        parent_node.append_child(first_child_node)
+        parent_node.append_child(second_child_node)
+        # Testing
+        parent_node.insert_before(second_child_node, first_child_node)
+        self.assertEqual(second_child_node.parent_node, parent_node)
+        self.assertEqual(parent_node.child_nodes.item(0), second_child_node)
+        self.assertEqual(parent_node.child_nodes.item(1), first_child_node)
+        self.assertEqual(parent_node.child_nodes.length, 2)
+
+    def test_InsertDocumentFragmentNode(self):
+        document = _create_document_node()
+        parent_node = _create_element_node(document)
+        docfrag_node = Node(node_type=NodeType.DOCUMENT_FRAGMENT_NODE,
+                            node_name='#document-fragment',
+                            owner_document=document)
+        first_child_node = _create_element_node(document)
+        second_child_node = _create_element_node(document)
+        # Warning: this is assuming that `append_child()` method works properly.
+        docfrag_node.append_child(first_child_node)
+        docfrag_node.append_child(second_child_node)
+        # Testing
+        parent_node.insert_before(docfrag_node)
+        self.assertEqual(parent_node.child_nodes.item(0), first_child_node)
+        self.assertEqual(parent_node.child_nodes.item(1), second_child_node)
+        self.assertEqual(parent_node.child_nodes.length, 2)
+
+    def test_Raises_HIERARCHY_REQUEST_ERR(self):
+        self.skipTest(NotImplemented)  # TODO
+
+    def test_Raises_WRONG_DOCUMENT_ERR(self):
+        document_1 = _create_document_node()
+        document_2 = _create_document_node()
+        elem_node_1 = _create_element_node(document_1)
+        elem_node_2 = _create_element_node(document_2)
+        try:
+            elem_node_1.insert_before(elem_node_2)
+        except DOMException as e:
+            code = e.args[0]
+            self.assertEqual(code, DOMException.WRONG_DOCUMENT_ERR)
+        else:
+            self.fail()
+
+    def test_Raises_NO_MODIFICATION_ALLOWED_ERR(self):
+        document = _create_document_node()
+        elem_node_readonly = Node(node_type=NodeType.ELEMENT_NODE,
+                                  node_name='tagName',
+                                  owner_document=document,
+                                  read_only=True)
+        elem_node_child = _create_element_node(document)
+        try:
+            elem_node_readonly.insert_before(elem_node_child)
+        except DOMException as e:
+            code = e.args[0]
+            self.assertEqual(code, DOMException.NO_MODIFICATION_ALLOWED_ERR)
+        else:
+            self.fail()
+
+    def test_Raises_NOT_FOUND_ERR(self):
+        document = _create_document_node()
+        elem_node_parent = _create_element_node(document)
+        elem_node_child = _create_element_node(document)
+        elem_node_error = _create_element_node(document)
+        # Testing
+        try:
+            elem_node_parent.insert_before(elem_node_child, elem_node_error)
+        except DOMException as e:
+            code = e.args[0]
+            self.assertEqual(code, DOMException.NOT_FOUND_ERR)
+        else:
+            self.fail()
 
 
 if __name__ == '__main__':
