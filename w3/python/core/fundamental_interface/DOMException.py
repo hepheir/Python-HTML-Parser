aaf3289596e3878f4f867709a2fd8e3d434da434
Hepheir <hepheir@gmail.com>
2021-06-08 00:01:36 +0900

    Update DOMException.py
    IDL Definition에 명시된 `unsigned short code` 가 존재하지 않아 추가하였음.
    또한, 상수 및 변수들에 `c_ushort`의 형 힌트를 추가하였음.

--- w3/python/core/fundamental_interface/DOMException.py
+++ w3/python/core/fundamental_interface/DOMException.py
@@ -1,33 +1,35 @@
-class ExceptionCode(int):
-    """Definition group `ExceptionCode`
-    An integer indicating the type of error generated.
-    """
-    INDEX_SIZE_ERR = 1
-    DOMSTRING_SIZE_ERR = 2
-    HIERARCHY_REQUEST_ERR = 3
-    WRONG_DOCUMENT_ERR = 4
-    INVALID_CHARACTER_ERR = 5
-    NO_DATA_ALLOWED_ERR = 6
-    NO_MODIFICATION_ALLOWED_ERR = 7
-    NOT_FOUND_ERR = 8
-    NOT_SUPPORTED_ERR = 9
-    INUSE_ATTRIBUTE_ERR = 10
+from typing import Final
+from ctypes import c_ushort
 
 
 class DOMException(Exception):
-    """Exception DOMException
+    """Exception `DOMException`
 
-    DOM operations only raise exceptions in "exceptional" circumstances, i.e., when an operation is impossible to perform (either for logical reasons, because data is lost, or because the implementation has become unstable). In general, DOM methods return specific error values in ordinary processing situation, such as out-of-bound errors when using `NodeList`.
-    Implementations may raise other exceptions under other circumstances. For example, implementations may raise an implementation-dependent exception if a null argument is passed.
-    Some languages and object systems do not support the concept of exceptions. For such systems, error conditions may be indicated using native error reporting mechanisms. For some bindings, for example, methods may return error codes similar to those listed in the corresponding method descriptions.
+    DOM operations only raise exceptions in "exceptional" circumstances, i.e., when an operation is impossible to perform
+    (either for logical reasons, because data is lost, or because the implementation has become unstable).
+    In general, DOM methods return specific error values in ordinary processing situation, such as out-of-bound errors when using `NodeList`.
+
+    Implementations may raise other exceptions under other circumstances.
+    For example, implementations may raise an implementation-dependent exception if a `None` argument is passed.
+
+    Some languages and object systems do not support the concept of exceptions.
+    For such systems, error conditions may be indicated using native error reporting mechanisms.
+    For some bindings, for example, methods may return error codes similar to those listed in the corresponding method descriptions.
     """
-    INDEX_SIZE_ERR = ExceptionCode.INDEX_SIZE_ERR
-    DOMSTRING_SIZE_ERR = ExceptionCode.DOMSTRING_SIZE_ERR
-    HIERARCHY_REQUEST_ERR = ExceptionCode.HIERARCHY_REQUEST_ERR
-    WRONG_DOCUMENT_ERR = ExceptionCode.WRONG_DOCUMENT_ERR
-    INVALID_CHARACTER_ERR = ExceptionCode.INVALID_CHARACTER_ERR
-    NO_DATA_ALLOWED_ERR = ExceptionCode.NO_DATA_ALLOWED_ERR
-    NO_MODIFICATION_ALLOWED_ERR = ExceptionCode.NO_MODIFICATION_ALLOWED_ERR
-    NOT_FOUND_ERR = ExceptionCode.NOT_FOUND_ERR
-    NOT_SUPPORTED_ERR = ExceptionCode.NOT_SUPPORTED_ERR
-    INUSE_ATTRIBUTE_ERR = ExceptionCode.INUSE_ATTRIBUTE_ERR
+
+    # Definition group `ExceptionCode`
+    # An integer indicating the type of error generated.
+    INDEX_SIZE_ERR: Final[c_ushort] = 1
+    DOMSTRING_SIZE_ERR: Final[c_ushort] = 2
+    HIERARCHY_REQUEST_ERR: Final[c_ushort] = 3
+    WRONG_DOCUMENT_ERR: Final[c_ushort] = 4
+    INVALID_CHARACTER_ERR: Final[c_ushort] = 5
+    NO_DATA_ALLOWED_ERR: Final[c_ushort] = 6
+    NO_MODIFICATION_ALLOWED_ERR: Final[c_ushort] = 7
+    NOT_FOUND_ERR: Final[c_ushort] = 8
+    NOT_SUPPORTED_ERR: Final[c_ushort] = 9
+    INUSE_ATTRIBUTE_ERR: Final[c_ushort] = 10
+
+    def __init__(self, error_code: c_ushort, *args: object) -> None:
+        super().__init__(*args)
+        self.code: c_ushort = error_code
