1873009711de04ed6db0bcf8088604016596ad91
Hepheir <hepheir@gmail.com>
2021-06-08 10:03:17 +0900

    Update __init__.py

--- w3/python/core/__init__.py
+++ w3/python/core/__init__.py
@@ -1,6 +1,6 @@
 """Module that contains classes of interfaces and ect."""
 
-from w3.python.core.DOMString import DOMString
+from w3.python.core.type import DOMString
 from w3.python.core.fundamental_interface import DOMException
 from w3.python.core.fundamental_interface import DOMImplementation
 from w3.python.core.fundamental_interface import Node
