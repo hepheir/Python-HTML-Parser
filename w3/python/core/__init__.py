c4c9a35072f3a9759d0021214a77c8d0e2c3ba71
김동주 <hepheir@gmail.com>
2021-06-09 12:58:36 +0900

    Delete two __init__.py files

--- w3/python/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Module that contains classes of interfaces and ect."""
-
-from w3.python.core.type import DOMString
-from w3.python.core.exception import DOMException
-from w3.python.core.fundamental_interface import DOMImplementation
-from w3.python.core.fundamental_interface import Node
-from w3.python.core.fundamental_interface import NodeType
-from w3.python.core.fundamental_interface import NodeList
